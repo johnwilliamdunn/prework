cdb61f1ab546cbd9700509bd06d4cc1f89680cb1
John Dunn <jdunn33@cnm.edu>
2019-06-15 20:32:44 -0600

    added example of basic mathmatic funtions in javascript

---
+++ demo/fundamentals/variables-and-data.js
@@ -0,0 +1,44 @@
+/*
+ *Variable, Data Types, and Typing
+ */
+let wordData;
+
+wordData = "Word List";
+
+console.log(wordData);
+
+let subtototal = 100;
+let tax = subtototal * 0.07;
+let totalwithtax = tax+subtototal;
+
+let obj - {
+	key1: "value",
+	key2: 4
+	boolean: true,
+	obj2: {
+		obj2Key1: "Internal Object Value",
+
+	}
+};
+
+console.log(obj.obj2.obj2Key2)
+
+let variable  : string = "string";
+
+let var1 ="2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "String 1" + "String 2";
+
+console.log (string);
+
+
