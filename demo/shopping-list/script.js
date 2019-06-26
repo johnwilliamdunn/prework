bafb69b567d4f4f343ed146fc3c582f77e67a6b7
John Dunn <jdunn33@cnm.edu>
2019-06-26 15:52:40 -0600

    added delete functionality, added random integer, fixed issue with let function.

--- demo/shopping-list/script.js
+++ demo/shopping-list/script.js
@@ -4,53 +4,62 @@ window.onload = function () {
 };
 
 function  initShoppingList () {
-let form = document.getElementById("item-form");
+	let form = document.getElementById('item-form');
 
-form.addEventListener("submit", (event) => {
-	handleItemForm (event, form);
-});
+	form.addEventListener('submit', (event) => {
+		handleItemForm(event, form);
+	});
 
-function handleItemForm(event, formRef) {
+}
+
+function handleItemForm(event, formReference) {
 	if(event.preventDefault) {
 		event.preventDefault();
 	}
 
 	addItemToShoppingList();
-	formRef.reset();
+
+	formReference.reset();
+
+}
 
 	function addItemToShoppingList() {
-		let itemName = document.getElementById("item-name");
-		let itemAmount = document.getElementById("item-amount");
+		let itemName = document.getElementById('item-name');
+		let itemAmount = document.getElementById('item-amount');
 		let id = getRandomInt(0, 10000000);
 
 		let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
-		let itemListRef = document.getElementById("shopping-list");
-		itemListRef.insertAdjacentHTML("afterend", itemHtml, id);
-	}
+		let itemListRef = document.getElementById('shopping-list');
+		itemListRef.insertAdjacentHTML('afterend', itemHtml);
 
-	setDeleteButtonEvent(id); {
+
+	setDeleteButtonEvent(id);
 
 	}
 
 
 function setDeleteButtonEvent(id) {
-	let deleteButton = document.getElementById("button"+id);
-	deleteButton.addEventListener("click", () => {
-		removeListItem(id);}
+	let deleteButton = document.getElementById('button'+id);
 
-function createListHtml(itemName, itemAmount, id) {
-		return `<li id="item${id}">
-           ${itemName} - ${itemAmount}
-		     <button id ="button${id}" type = "button">Delete Item</button>
+	deleteButton.addEventListener('click', () => {
+		removeListItem(id);
+})
+
+}
+
+function createListItemHtml(itemName, itemAmount, id) {
+		return `<li id="item${id}">${itemName} - ${itemAmount}
+		     <button id ="button${id}" type="button">Delete Item</button>
 	       </li>`;
 }
 
    function removeListItem(id) {
-		let listItem = document.getElementById("item" + id);
+		let listItem = document.getElementById('item' + id);
 		listItem.parentNode.removeChild(listItem);
 	}
+
 	function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 		return Math.floor(Math.random() * (max- min)) + min;
-	};
+	}
